1a5d166658bcd5f3437c663e8d3b4bbb833f6489
Alejandro Guillermo Garcia Mancilla <alejandrogarciamancilla@gmail.com>
2025-11-23 09:32:07 -0400

    feat:agregar los routes

--- src/modules/chat/routes/chat.route.js
+++ src/modules/chat/routes/chat.route.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { getMessages, createOrGetConversation , listUsuarios, crearConversacionPrivada, getConversacion, crearGrupo} from "../controllers/chat.controller.js";
+import { getMessages, createOrGetConversation , listUsuarios, crearConversacionPrivada, getConversacion, crearGrupo, agregarParticipanteAGrupo, eliminarParticipanteDeGrupo, listarParticipantes} from "../controllers/chat.controller.js";
 const router = Router();
 
 router.post("/conversations/get-or-create", createOrGetConversation);
-
 
 router.get("/conversations/:id/messages", getMessages);
 router.get("/usuarios", listUsuarios);
 router.post("/conversacion/privada", crearConversacionPrivada);
 router.get("/conversacion/:id", getConversacion);
 router.post("/conversacion/grupal", crearGrupo);
+router.post("/conversacion/:conversacionId/agregar-participante", agregarParticipanteAGrupo);
+router.post("/conversacion/:conversacionId/eliminar-participante", eliminarParticipanteDeGrupo);
+router.get("/conversacion/:conversacionId/participantes", listarParticipantes);
 export default router;
