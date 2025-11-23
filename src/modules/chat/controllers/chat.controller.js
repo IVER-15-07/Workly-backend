71885100800551bb1e36a8e11fc66fa1160439a2
iver <limber123iver654.12@gmail.com>
2025-11-23 09:16:49 -0400

    fix service de chat borre servicios que no hacian falta

--- src/modules/chat/controllers/chat.controller.js
+++ src/modules/chat/controllers/chat.controller.js
@@ -15,20 +15,7 @@ export async function crearConversacionGrupal(req, res) {
   }
 }
 
-export async function createOrGetConversation(req, res) {
-  try {
-    const { usuario1id, usuario2id } = req.body;
-    const conversacion = await ChatService.crearORecuperarConversacion(Number(usuario1id), Number(usuario2id));
-    return res.status(200).json({ success: true, data: conversacion });
-  } catch (err) {
-    console.error("Error en crear o recuperar conversación:", err);
-    return res.status(err.status || 500).json({
-      success: false,
-      message: err.message || "Error interno del servidor",
-      data: null
-    });
-  }
-};
+
 
 export async function getMessages(req, res) {
   try {
--- src/modules/chat/routes/chat.route.js
+++ src/modules/chat/routes/chat.route.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { getMessages, createOrGetConversation , listUsuarios, crearConversacionPrivada, getConversacion, crearGrupo} from "../controllers/chat.controller.js";
+import { getMessages, listUsuarios, crearConversacionPrivada, getConversacion, crearGrupo, crearConversacionGrupal} from "../controllers/chat.controller.js";
 const router = Router();
-
-router.post("/conversations/get-or-create", createOrGetConversation);
-
 
 router.get("/conversations/:id/messages", getMessages);
 router.get("/usuarios", listUsuarios);
 router.post("/conversacion/privada", crearConversacionPrivada);
+router.post("/conversacion/grupal", crearConversacionGrupal);
 router.get("/conversacion/:id", getConversacion);
 router.post("/conversacion/grupal", crearGrupo);
+
 export default router;
